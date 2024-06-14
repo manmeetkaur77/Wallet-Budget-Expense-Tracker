2491fcf6c7772429babe29cf8f345c6efcf13575
manmeetkaur77 <manmeetkaur1629@gmail.com>
2024-06-14 13:19:33 +0530

    server setup

--- Backened/app.js
+++ Backened/app.js
@@ -1,26 +1,19 @@
 
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+console.log("hello friend");
 
-require('dotenv').config()
+const express = require('express');
+const cors=require('cors');
+const app =express();
+require ('dotenv').config();
+const Port=process.env.PORT;
 
-const PORT = process.env.PORT
-
-//middlewares
-app.use(express.json())
-app.use(cors())
-
-//routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
-
-const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
-
-server()
+app.use(express.json());
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('Hello from node js');
+})
+const server = () =>{
+   app.listen(Port,()=>{
+    console.log(`Server is running on port ${Port}`);
+   })};
+server();
